1a6ad67cb5b3f4ffeb1f3e550568e5ee24d19310
Kochiyama <dev@marcelokochiyama.com>
2025-01-24 13:11:09 -0400

    feat: add better icons

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -6,8 +6,7 @@ return {
 		local dashboard = require("alpha.themes.dashboard")
 
 		dashboard.section.header.val = require("ascii").art.text.neovim.sharp
-
-		-- dashboard.section.header.val = {
+    -- dashboard.section.header.val = {
 		--   "",
 		--   "",
 		--   "",
@@ -31,24 +30,27 @@ return {
 		--   '         "Y888P"                                             ',
 		-- }
 
-		dashboard.section.buttons.val = {
-			dashboard.button("f", " Find file", ":Telescope find_files <CR>"),
-			dashboard.button("e", " New file", ":ene <BAR> startinsert <CR>"),
-			dashboard.button("r", " Recently used files", ":Telescope oldfiles <CR>"),
-			dashboard.button("t", "󱎸 Find text", ":Telescope live_grep <CR>"),
-			dashboard.button("c", " Configuration", ":e ~/.config/nvim<CR>"),
-			dashboard.button("q", "⏻ Quit Neovim", ":qa<CR>"),
-		}
+		-- dashboard.section.buttons.val = {
+		-- 	dashboard.button("f", " Find file", ":Telescope find_files <CR>"),
+		-- 	dashboard.button("e", " New file", ":ene <BAR> startinsert <CR>"),
+		-- 	dashboard.button("r", " Recently used files", ":Telescope oldfiles <CR>"),
+		-- 	dashboard.button("t", "󱎸 Find text", ":Telescope live_grep <CR>"),
+		-- 	dashboard.button("c", " Configuration", ":e ~/.config/nvim<CR>"),
+		-- 	dashboard.button("q", "⏻ Quit Neovim", ":qa<CR>"),
+		-- }
+
+    dashboard.section.buttons.val = {}
 
 		local function footer()
-			return "Think. Design. Develop."
+			return "\nCode is like humor. When you have to explain it, it’s bad."
 		end
+
 		dashboard.section.footer.val = footer()
 
-		dashboard.section.footer.opts.hl = "@constant"
-		dashboard.section.header.opts.hl = "@parameter"
-		dashboard.section.buttons.opts.hl = "Keyword"
-		dashboard.section.buttons.opts.noautocmd = false
+		-- dashboard.section.footer.opts.hl = "@constant"
+		-- dashboard.section.header.opts.hl = "@parameter"
+		-- dashboard.section.buttons.opts.hl = "Keyword"
+		-- dashboard.section.buttons.opts.noautocmd = false
 
 		alpha.setup(dashboard.opts)
 	end,
