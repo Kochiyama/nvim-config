4d04a9611f5393015ad0da5523010cc7a2d2e268
Kochiyama <dev@marcelokochiyama.com>
2025-02-05 20:24:32 -0400

    fix signcolumn background

--- lua/plugins/nord.lua
+++ lua/plugins/nord.lua
@@ -1,7 +1,7 @@
 return {
 	"arcticicestudio/nord-vim",
 	config = function()
-		vim.g.nord_bold_vertical_split_line = 1
+		vim.g.nord_bold_vertical_split_line = 0
 		vim.g.nord_uniform_diff_background = 1
 		vim.g.nord_italic = 1
 		vim.g.nord_italic_comments = 1
@@ -9,5 +9,6 @@ return {
 		vim.g.nord_disable_background = true
 		vim.cmd("colorscheme nord")
 		vim.cmd("highlight Normal ctermbg=none guibg=none")
+		vim.cmd("highlight SignColumn ctermbg=none guibg=none")
 	end,
 }
