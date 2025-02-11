aa2d6544e13619d1df3d0080807dbbea7bdc450f
Kochiyama <dev@marcelokochiyama.com>
2025-02-11 09:00:35 -0400

    add descriptions to lsp keybindings

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,131 +1,131 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "prismals",
-          "rust_analyzer",
-          "ts_ls",
-          "tailwindcss",
-          "docker_compose_language_service",
-          "dockerls",
-          "marksman",
-          "sqlls",
-          "taplo",
-          "yamlls",
-          "gopls",
-          "pyright",
-          "eslint",
-          "emmet_ls",
-          "eslint",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"prismals",
+					"rust_analyzer",
+					"ts_ls",
+					"tailwindcss",
+					"docker_compose_language_service",
+					"dockerls",
+					"marksman",
+					"sqlls",
+					"taplo",
+					"yamlls",
+					"gopls",
+					"pyright",
+					"eslint",
+					"emmet_ls",
+					"eslint",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-      local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.emmet_ls.setup({
-        capabilities = capabilities,
-        filetypes = {
-          "css",
-          "eruby",
-          "html",
-          "javascript",
-          "javascriptreact",
-          "less",
-          "sass",
-          "scss",
-          "svelte",
-          "pug",
-          "typescriptreact",
-          "vue",
-        },
-        init_options = {
-          html = {
-            options = {
-              -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-              ["bem.enabled"] = true,
-            },
-          },
-        },
-      })
-      lspconfig.mdx_analyzer.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.prismals.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-        on_attach = function(client)
-          if client.resolved_capabilities then
-            client.resolved_capabilities.document_formatting = false
-          end
-        end,
-      })
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.docker_compose_language_service.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.dockerls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.marksman.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.sqlls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.taplo.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.yamlls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
+			lspconfig.cssls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.emmet_ls.setup({
+				capabilities = capabilities,
+				filetypes = {
+					"css",
+					"eruby",
+					"html",
+					"javascript",
+					"javascriptreact",
+					"less",
+					"sass",
+					"scss",
+					"svelte",
+					"pug",
+					"typescriptreact",
+					"vue",
+				},
+				init_options = {
+					html = {
+						options = {
+							-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+							["bem.enabled"] = true,
+						},
+					},
+				},
+			})
+			lspconfig.mdx_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.prismals.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ts_ls.setup({
+				capabilities = capabilities,
+				on_attach = function(client)
+					if client.resolved_capabilities then
+						client.resolved_capabilities.document_formatting = false
+					end
+				end,
+			})
+			lspconfig.tailwindcss.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.docker_compose_language_service.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.dockerls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.marksman.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.sqlls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.taplo.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.gopls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
 
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {})
-      vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>lf", function()
-        vim.lsp.buf.format({ async = true })
-      end, {})
-    end,
-  },
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Goto Declaration" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Info" })
+			vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, { desc = "Rename" })
+			vim.keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, { desc = "Code Actions" })
+			vim.keymap.set("n", "<leader>lf", function()
+				vim.lsp.buf.format({ async = true })
+			end, { desc = "Format" })
+		end,
+	},
 }
