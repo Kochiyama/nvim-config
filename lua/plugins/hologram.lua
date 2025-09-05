1a33c1a9f77f572aa1aa18b911dd281447a9ca9c
Kochiyama <dev@marcelokochiyama.com>
2025-09-05 15:34:47 -0300

    remove hologram

--- lua/plugins/hologram.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'edluffy/hologram.nvim',
-  config = function()
-    require('hologram').setup {
-      auto_display = false -- WIP automatic markdown image display, may be prone to breaking
-    }
-  end
-}
